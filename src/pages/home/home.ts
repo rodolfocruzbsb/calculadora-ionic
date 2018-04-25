e01cc535bdfa42808b57652e8581328244b74a0f
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2018-04-25 14:47:34 -0300

    Criação de APP de calculadora

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,70 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  numeroUm: string;
+  numeroDois: string;
 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  somar() {
+    if (this.validarDados()) {
+      let resultado = parseInt(this.numeroUm) + parseInt(this.numeroDois);
+      this.mostrarAlerta(resultado.toString());
+    }
+  }
+
+  subtrair() {
+    if (this.validarDados()) {
+      let resultado = parseInt(this.numeroUm) - parseInt(this.numeroDois);
+      this.mostrarAlerta(resultado.toString());
+    }
+  }
+
+  multiplicar() {
+    if (this.validarDados()) {
+      let resultado = parseInt(this.numeroUm) * parseInt(this.numeroDois);
+      this.mostrarAlerta(resultado.toString());
+    }
+  }
+
+  dividir() {
+    if (this.validarDados()) {
+      if(this.numeroDois === '0'){
+        this.mostrarErro('Não é possível dividir por ZERO!')
+        return false;
+      }
+      let resultado = parseInt(this.numeroUm) / parseInt(this.numeroDois);
+      this.mostrarAlerta(resultado.toString());
+    }
+  }
+
+  validarDados() {
+    if (this.numeroUm == null || this.numeroUm.length == 0
+      || this.numeroUm == null || this.numeroUm.length == 0) {
+      this.mostrarErro('É obrigatório informar os dois números');
+      return false;
+    }
+    return true;
+  }
+
+  mostrarAlerta(valor: string) {
+    let alert = this.alertCtrl.create({
+      title: 'Resultado!',
+      subTitle: `O resultado da operação é: ${valor}`,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  mostrarErro(mensagem: string) {
+    let alert = this.alertCtrl.create({
+      title: 'Erro!',
+      subTitle: `Tá erradoooooooo: ${mensagem}`,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
